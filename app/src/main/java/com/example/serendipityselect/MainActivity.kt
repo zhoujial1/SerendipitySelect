a813c122eac7f3e6f36742360c860f3623865f22
Jackie <zhoujial1@ecs.vuw.ac.nz>
2023-09-02 12:40:08 +1200

    Added layout and randomizer

--- app/src/main/java/com/example/serendipityselect/MainActivity.kt
+++ app/src/main/java/com/example/serendipityselect/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.serendipityselect
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,17 +14,29 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.serendipityselect.ui.theme.SerendipitySelectTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
+        /*setContent {
             SerendipitySelectTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Greeting("Android")
                 }
             }
+        }*/
+        setContentView(R.layout.activity_main)
+
+        val rollButton = findViewById<Button>(R.id.rollButton)
+        val result = findViewById<TextView>(R.id.result)
+        val seekBar = findViewById<SeekBar>(R.id.seekBar)
+
+        rollButton.setOnClickListener{
+            val random = Random.nextInt(seekBar.progress) + 1
+            result.text = random.toString()
+
         }
     }
 }
